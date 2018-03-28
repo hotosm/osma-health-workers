e3c386a7a6703563d52d7d3c0b946afb35554c44
Marc Farra <marcfarra@gmail.com>
2018-03-28 19:18:14 -0400

    Add country as command line argument

--- run.sh
+++ run.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# run all workers based on countries.list
-
 TilesURL="https://s3.amazonaws.com/mapbox/osm-qa-tiles-production/latest.country"
 WORKDIR="data"
+COUNTRY=$1
 
-while read -r line
-do
-    country="$line"
-    echo "processing $country"
-    
-    # wget "$TilesURL/$country.mbtiles.gz"
-    gunzip "$country.mbtiles.gz"
 
-    ./workers/temporal.sh $country $WORKDIR
-done < "countries.list"
+wget "$TilesURL/$COUNTRY.mbtiles.gz"
+gunzip "$COUNTRY.mbtiles.gz"
+
+./workers/temporal.sh $COUNTRY $WORKDIR
 
