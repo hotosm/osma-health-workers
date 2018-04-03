0aaddc7904cf1a10f87bc39048a4aebc16a212f0
Sajjad Anwar <sajjadkm@gmail.com>
2018-04-03 17:20:11 +0530

    define countries file structure. run attribute completeness for each boundary

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 TilesURL="https://s3.amazonaws.com/mapbox/osm-qa-tiles-production/latest.country"
+Countries="https://gist.githubusercontent.com/geohacker/a0f2c9d7ad2a7f90dff0a360e3444ef6/raw/3ae79d2144ba2e8ba890f3a17e5bc5b76d2dd69c/countries.json"
 WORKDIR="data"
 COUNTRY=$1
 
 
 wget "$TilesURL/$COUNTRY.mbtiles.gz"
+wget "$Countries"
 gunzip "$COUNTRY.mbtiles.gz"
 
 ./workers/temporal.sh $COUNTRY $WORKDIR
--- workers/attribute-completeness.js
+++ workers/attribute-completeness.js
@@ -5,20 +5,44 @@
 
 const fs = require('fs');
 const path = require('path');
-const osmlint = require('../node_modules/osmlint')
+const osmlint = require('../node_modules/osmlint');
+const turfBbox = require('../node_modules/@turf/bbox').default;
 const argv = require('../node_modules/minimist')(process.argv.slice(2));
 const country = argv._[0];
 const workdir = argv._[1];
 const mbtilesPath = country + '.mbtiles';
 
-// run osmlint and store stats in the workdir
+// run osmlint for the entire country
 osmlint.incompleteResidentialBuildings({zoom: 12}, mbtilesPath, (err, data) => {
     if (err) {
         console.error('Error:', err);
         return;
     }
-    fs.writeFileSync(workdir + '/' + country + '/building-stats.json', JSON.stringify(data), {'encoding': 'utf-8'});
+    const countryLocation = workdir + '/' + country;
+    if (!fs.existsSync(countryLocation)) {
+        fs.mkdirSync(countryLocation);
+    }
+    fs.writeFileSync(countryLocation + '/building-stats.json', JSON.stringify(data), {'encoding': 'utf-8'});
 });
 
+// read country boundaries
+const countries = JSON.parse(fs.readFileSync('countries.json'), {'encoding': 'utf-8'});
+const boundaries = countries[country];
 
+boundaries.features.forEach((b) => {
+    const bbox = turfBbox(b);
+    const name = b.properties.name.toLowerCase();
 
+    // run osmlint for each of the boundary and store stats in the workdir
+    osmlint.incompleteResidentialBuildings({zoom: 12, bbox: bbox}, mbtilesPath, (err, data) => {
+        if (err) {
+            console.error('Error:', err);
+            return;
+        }
+        const boundaryLocation = workdir + '/' + country + '/' + name;
+        if (!fs.existsSync(boundaryLocation)) {
+            fs.mkdirSync(boundaryLocation);
+        }
+        fs.writeFileSync(boundaryLocation + '/building-stats.json', JSON.stringify(data), {'encoding': 'utf-8'});
+    });
+});
