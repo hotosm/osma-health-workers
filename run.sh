9376fe5d684778b0ad5848520d8aa2ed9a55aa9e
Sajjad Anwar <sajjadkm@gmail.com>
2018-04-04 19:06:33 +0530

    edit recency

--- run.sh
+++ run.sh
@@ -5,8 +5,14 @@ WORKDIR="data"
 COUNTRY=$1
 
 
-wget "$TilesURL/$COUNTRY.mbtiles.gz"
-gunzip "$COUNTRY.mbtiles.gz"
+# wget "$TilesURL/$COUNTRY.mbtiles.gz"
+# gunzip "$COUNTRY.mbtiles.gz"
 
+# extract all the residential buildings
 ./workers/temporal.sh $COUNTRY $WORKDIR
+
+# run attribute completeness validator and get stats
 node ./workers/attribute-completeness.js $COUNTRY $WORKDIR
+
+# filter all residential buildings and get edit receny stats
+node ./workers/edit-recency.js $COUNTRY $WORKDIR
