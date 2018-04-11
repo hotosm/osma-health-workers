46be983b9340da7a968fd5a1675d64402cfd6367
Sajjad Anwar <sajjadkm@gmail.com>
2018-04-11 19:04:45 +0530

    download completeness stats and create a tileset

--- run.sh
+++ run.sh
@@ -20,6 +20,12 @@ node ./workers/attribute-completeness.js $COUNTRY $WORKDIR
 # filter all residential buildings and get edit receny stats
 node ./workers/edit-recency.js $COUNTRY $WORKDIR
 
+# download map completeness json
+aws s3 cp s3://hot-osm/$COUNTRY-predictions.json $WORKDIR/$COUNTRY
+
+# create tileset for map completeness
+node ./workers/map-completeness-tiles.js $WORKDIR/$COUNTRY/$COUNTRY-predictions.json | tippecanoe -l -f -o /$WORKDIR/$COUNTRY/map-completeness.mbtiles
+
 # run stats for duplicate buildings
 node ./workers/duplicate-buildings.js $COUNTRY $WORKDIR > /dev/null
 
