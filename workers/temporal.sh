c5ff628097af34f1eb3b3d58d69faf1667c1708e
Marc Farra <marcfarra@gmail.com>
2018-04-12 17:09:53 -0400

    Change residential building to non-residential

--- workers/temporal.sh
+++ workers/temporal.sh
@@ -7,4 +7,4 @@ COUNTRY=$1
 WORKDIR=$2
 echo "running temporal for $COUNTRY"
 mkdir -p $WORKDIR/$COUNTRY/
-node node_modules/osmlint/cli.js filterresidentialbuildings --zoom=12 "$COUNTRY.mbtiles" > $WORKDIR/$COUNTRY/buildings.json
+node node_modules/osmlint/cli.js filterbuildings --zoom=12 "$COUNTRY.mbtiles" > $WORKDIR/$COUNTRY/buildings.json
