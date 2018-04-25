96e8cbbd30bdfe29457fc90d7291c1440b083237
Marc Farra <marcfarra@gmail.com>
2018-04-25 15:42:16 -0400

    Pull out numbers to flatten object in tile

--- workers/map-completeness-tiles.js
+++ workers/map-completeness-tiles.js
@@ -13,7 +13,15 @@ Object.keys(estimates).forEach((key) => {
     let tile = key.split('/');
     tile = [parseInt(tile[1]), parseInt(tile[2]), parseInt(tile[0])];
     let geom = turfHelpers.feature(tilebelt.tileToGeoJSON(tile));
-    geom.properties = estimates[key];
+    let {index, actual, prediction} = estimates[key];
+    geom.properties = {
+        index,
+        actual_pop_sum: actual.pop_sum,
+        actual_osm_sum: actual.osm_sum,
+        actual_osm_avg: actual.osm_avg,
+        predicted_osm_sum: prediction.osm_sum,
+        predicted_osm_avg: predction.osm_avg
+    };
     data.push(geom);
     console.log(JSON.stringify(geom));
 });
